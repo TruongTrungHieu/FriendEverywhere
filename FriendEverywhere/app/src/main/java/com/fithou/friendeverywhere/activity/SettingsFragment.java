df29408f4476adfeb4f62e7996306ee7c87b1de5
vuhuyhung1304 <vuhuyhung1304@gmail.com>
2016-03-22 22:57:10 +0700

    ProfileActivity, Fragment_setting

--- FriendEverywhere/app/src/main/java/com/fithou/friendeverywhere/activity/SettingsFragment.java
+++ FriendEverywhere/app/src/main/java/com/fithou/friendeverywhere/activity/SettingsFragment.java
@@ -1,20 +1,36 @@
 package com.fithou.friendeverywhere.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.fithou.friendeverywhere.R;
 
 public class SettingsFragment extends Fragment implements View.OnClickListener {
 
+    private Button btn_suaInfo, btn_accSettings, btn_aboutUs, btn_feedback, btn_logout;
+    private ImageView img_avatar;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         // TODO Auto-generated method stub
         super.onCreate(savedInstanceState);
+    }
+
+    protected void inflateView(){
+/*        btn_aboutUs = Button.OnClickListener();
+        btn_accSettings = (Button)findViewById();
+        btn_feedback = (Button)findViewById();
+        btn_logout = (Button)findViewById();
+        btn_suaInfo = (Button)findViewById();
+
+        img_avatar = (ImageView)findViewById(R.id.iv_profileactivity);*/
     }
 
     @Override
@@ -30,4 +46,6 @@ public class SettingsFragment extends Fragment implements View.OnClickListener {
 
     }
 
+    //Intent t = new Intent(SettingsFragment.this, ProfileActivity.class);
+    //startActivity(t);
 }
