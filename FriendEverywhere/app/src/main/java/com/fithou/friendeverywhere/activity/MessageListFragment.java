abe6a2d3546fa076265f7786b65df6c233806d83
vuhuyhung1304 <vuhuyhung1304@gmail.com>
2016-03-24 19:04:56 +0700

    MessageList has showed items

--- FriendEverywhere/app/src/main/java/com/fithou/friendeverywhere/activity/MessageListFragment.java
+++ FriendEverywhere/app/src/main/java/com/fithou/friendeverywhere/activity/MessageListFragment.java
@@ -3,20 +3,33 @@ package com.fithou.friendeverywhere.activity;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.fithou.friendeverywhere.R;
+import com.fithou.friendeverywhere.adapter.MessageListAdapter;
+import com.fithou.friendeverywhere.object.MessageObject;
+import com.fithou.friendeverywhere.object.UserObject;
+
+import java.util.ArrayList;
 
 public class MessageListFragment extends Fragment implements View.OnClickListener {
 
     private FloatingActionButton btn_new_message;
+    private RecyclerView rv_mess_list;
+    private MessageListAdapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private ArrayList<MessageObject> listMessage;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         // TODO Auto-generated method stub
         super.onCreate(savedInstanceState);
+
     }
 
     @Override
@@ -24,14 +37,79 @@ public class MessageListFragment extends Fragment implements View.OnClickListene
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_messagelist, container, false);
 
-        btn_new_message = (FloatingActionButton)v.findViewById(R.id.btn_new_message);
+        btn_new_message = (FloatingActionButton) v.findViewById(R.id.btn_new_message);
+        rv_mess_list = (RecyclerView) v.findViewById(R.id.rv_message_list);
+
+        initData();
+        reloadData();
+
 
         return v;
     }
+
+    public void reloadData() {
+        if (listMessage != null) {
+            rv_mess_list.setHasFixedSize(true);
+
+            layoutManager = new LinearLayoutManager(this.getActivity());
+            rv_mess_list.setLayoutManager(layoutManager);
+
+            adapter = new MessageListAdapter(listMessage, this.getActivity());
+            rv_mess_list.setAdapter(adapter);
+        }
+    }
+
 
     @Override
     public void onClick(View v) {
 
     }
 
+    public void initData() {
+        listMessage = new ArrayList<>();
+        MessageObject messageObject = new MessageObject();
+        UserObject userObject = new UserObject();
+        messageObject.setContent("dafjaskdjfhasjkdfhajkdshfjkahdfjkahdf");
+        userObject.setFullname("Nguyen Tran Dung");
+        messageObject.setUserObject(userObject);
+        listMessage.add(messageObject);
+
+        MessageObject messageObject1 = new MessageObject();
+        UserObject userObject1 = new UserObject();
+        messageObject1.setContent("dafjaskdjfhasjkdfhajkdshfjkahdadsfjhadsjkfhajkfhjkdhfjkadshfjkadshfjkhadsfjkhasdjkfhsajkfhdasjkhdfdasjkhfjkashdfjkashdfkjhmndfmadsfjkfjkahdf");
+        userObject1.setFullname("Nguyen Tran Dung");
+        messageObject1.setUserObject(userObject1);
+        listMessage.add(messageObject1);
+        listMessage.add(messageObject1);
+        listMessage.add(messageObject1);
+        listMessage.add(messageObject1);
+        listMessage.add(messageObject1);
+        listMessage.add(messageObject1);
+        listMessage.add(messageObject1);listMessage.add(messageObject1);
+        listMessage.add(messageObject1);
+        listMessage.add(messageObject1);
+        listMessage.add(messageObject1);
+        listMessage.add(messageObject1);
+        listMessage.add(messageObject1);
+        listMessage.add(messageObject1);listMessage.add(messageObject1);
+        listMessage.add(messageObject1);
+        listMessage.add(messageObject1);
+        listMessage.add(messageObject1);
+        listMessage.add(messageObject1);
+        listMessage.add(messageObject1);
+        listMessage.add(messageObject1);listMessage.add(messageObject1);
+        listMessage.add(messageObject1);
+        listMessage.add(messageObject1);
+        listMessage.add(messageObject1);
+        listMessage.add(messageObject1);
+        listMessage.add(messageObject1);
+        listMessage.add(messageObject1);listMessage.add(messageObject1);
+        listMessage.add(messageObject1);
+        listMessage.add(messageObject1);
+        listMessage.add(messageObject1);
+        listMessage.add(messageObject1);
+        listMessage.add(messageObject1);
+        listMessage.add(messageObject1);
+
+    }
 }
---
+++ FriendEverywhere/app/src/main/java/com/fithou/friendeverywhere/adapter/MessageListAdapter.java
@@ -0,0 +1,70 @@
+package com.fithou.friendeverywhere.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.fithou.friendeverywhere.R;
+import com.fithou.friendeverywhere.object.CountryObject;
+import com.fithou.friendeverywhere.object.MessageObject;
+
+import java.util.ArrayList;
+
+/**
+ * Created by admin on 24/03/2016.
+ */
+public class MessageListAdapter extends RecyclerView.Adapter<MessageListAdapter.RecyclerViewHolder> {
+
+    private ArrayList<MessageObject> listData = new ArrayList<>();
+
+    public MessageListAdapter(ArrayList<MessageObject> listData, Context c) {
+        this.listData = listData;
+    }
+
+    @Override
+    public int getItemCount() {
+        return listData.size();
+    }
+
+    @Override
+    public RecyclerViewHolder onCreateViewHolder(ViewGroup viewGroup,
+                                                 int position) {
+        LayoutInflater inflater = LayoutInflater.from(viewGroup.getContext());
+        View itemView = inflater.inflate(R.layout.message_list_item, viewGroup, false);
+        return new RecyclerViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerViewHolder viewHolder, int position) {
+        MessageObject messageObject = listData.get(position);
+        viewHolder.tv_message_list_name.setText(messageObject.getUserObject().getFullname());
+        viewHolder.tv_message_list_content.setText(messageObject.getContent());
+    }
+
+    public class RecyclerViewHolder extends RecyclerView.ViewHolder implements
+            View.OnClickListener {
+
+        public View line;
+        public TextView tv_message_list_name;
+        public TextView tv_message_list_content;
+        public ImageView img_message_list_ava;
+
+        public RecyclerViewHolder(View itemView) {
+            super(itemView);
+            line = (View)itemView.findViewById(R.id.line_message_list);
+            img_message_list_ava = (ImageView)itemView.findViewById(R.id.iv_mess_list_item);
+            tv_message_list_name = (TextView)itemView.findViewById(R.id.tv_name_mess_list_item);
+            tv_message_list_content = (TextView)itemView.findViewById(R.id.tv_content_mess_list_item);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+
+        }
+    }
+}
