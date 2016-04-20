7525723d5740bb903bc9c47e9f6b6f12176a4f4f
vuhuyhung1304 <vuhuyhung1304@gmail.com>
2016-04-20 13:47:20 +0700

    MapFragment 50%

--- FriendEverywhere/app/src/main/java/com/fithou/friendeverywhere/activity/MapFragment.java
+++ FriendEverywhere/app/src/main/java/com/fithou/friendeverywhere/activity/MapFragment.java
@@ -1,27 +1,36 @@
 package com.fithou.friendeverywhere.activity;
 
+import android.app.Activity;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.fithou.friendeverywhere.R;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapFragment extends Fragment implements OnMapReadyCallback {
 
     private GoogleMap googleMap;
     private MapView mMapView;
+    private View marker, view_btm;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -39,6 +48,13 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
 
         mMapView.getMapAsync(this);
 
+        UiSettings settings = mMapView.getMap().getUiSettings();
+        settings.setMapToolbarEnabled(false);
+
+        marker = ((LayoutInflater) this.getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE)).inflate(R.layout.custom_marker_layout, null);
+        TextView numTxt = (TextView) marker.findViewById(R.id.num_txt);
+        numTxt.setText("27");
+
         return v;
     }
 
@@ -51,7 +67,11 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
         googleMap.setIndoorEnabled(true);
         googleMap.setBuildingsEnabled(true);
 
-        googleMap.addMarker(new MarkerOptions().position(new LatLng(21.027019, 105.838165)));
+        googleMap.addMarker(new MarkerOptions()
+                .position(new LatLng(21.027019, 105.838165))
+                .title("Title")
+                .snippet("Description")
+                .icon(BitmapDescriptorFactory.fromBitmap(createDrawableFromView(this.getActivity(), marker))));
         googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(21.027019, 105.838165), 13));
     }
 
@@ -71,6 +91,21 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
     public void onLowMemory() {
         super.onLowMemory();
         mMapView.onLowMemory();
+    }
+
+    public static Bitmap createDrawableFromView(Context context, View view) {
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        ((Activity) context).getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        view.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        view.measure(displayMetrics.widthPixels, displayMetrics.heightPixels);
+        view.layout(0, 0, displayMetrics.widthPixels, displayMetrics.heightPixels);
+        view.buildDrawingCache();
+        Bitmap bitmap = Bitmap.createBitmap(view.getMeasuredWidth(), view.getMeasuredHeight(), Bitmap.Config.ARGB_8888);
+
+        Canvas canvas = new Canvas(bitmap);
+        view.draw(canvas);
+
+        return bitmap;
     }
 
 }
